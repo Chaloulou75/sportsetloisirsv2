18f8687ff2e35b06ed59ad18fae5e6c5c8fa44f9
Chaloulou75 <c.jeandey@gmail.com>
2023-05-23 23:20:10 +0200

    tarifs start

---
+++ app/Models/StructureTarif.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+
+class StructureTarif extends Model
+{
+    use HasFactory;
+
+    protected $table = 'structures_tarifs';
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $guarded = [];
+
+    public function structure(): BelongsTo
+    {
+        return $this->belongsTo(Structure::class);
+    }
+
+    public function categorie(): BelongsTo
+    {
+        return $this->belongsTo(LienDisciplineCategorie::class, 'categorie_id');
+    }
+
+    public function discipline(): BelongsTo
+    {
+        return $this->belongsTo(ListDiscipline::class, 'discipline_id');
+    }
+
+    public function activite(): BelongsTo
+    {
+        return $this->belongsTo(StructureActivite::class, 'activite_id');
+    }
+
+    public function produit(): BelongsTo
+    {
+        return $this->belongsTo(StructureProduit::class, 'produit_id');
+    }
+    //tarif type
+}
---
+++ resources/js/Components/Modals/ModalAddTarif.vue
@@ -0,0 +1,293 @@
+<script setup>
+import { useForm, router } from "@inertiajs/vue3";
+import { ref } from "vue";
+import { XCircleIcon } from "@heroicons/vue/24/outline";
+import {
+    TransitionRoot,
+    TransitionChild,
+    Dialog,
+    DialogPanel,
+    DialogTitle,
+    Listbox,
+    ListboxButton,
+    ListboxOptions,
+    ListboxOption,
+} from "@headlessui/vue";
+
+const emit = defineEmits("close");
+
+const props = defineProps({
+    errors: Object,
+    structure: Object,
+    show: Boolean,
+});
+
+const uniteDurees = [
+    { id: 1, name: "Heures" },
+    { id: 2, name: "Jours" },
+    { id: 3, name: "Mois" },
+    { id: 4, name: "Années" },
+];
+const selectedUniteeDuree = ref(uniteDurees[0]);
+
+const formAddTarif = useForm({
+    structure_id: ref(props.structure.id),
+    titre: ref(null),
+    description: ref(null),
+});
+
+const onSubmitAddTarifForm = () => {
+    router.post(
+        `/structures/${props.structure.slug}/tarifs`,
+        {
+            structure_id: formAddTarif.structure_id,
+            titre: formAddTarif.titre,
+            description: formAddTarif.description,
+        },
+        {
+            preserveScroll: true,
+            remember: false,
+            onSuccess: () => {
+                formAddTarif.reset();
+                emit("close");
+            },
+            structure: props.structure.slug,
+        }
+    );
+};
+</script>
+<template>
+    <div>
+        <TransitionRoot appear :show="show" as="template">
+            <Dialog as="div" @close="open = false" class="relative z-10">
+                <TransitionChild
+                    as="template"
+                    enter="duration-300 ease-out"
+                    enter-from="opacity-0"
+                    enter-to="opacity-100"
+                    leave="duration-200 ease-in"
+                    leave-from="opacity-100"
+                    leave-to="opacity-0"
+                >
+                    <div class="fixed inset-0 bg-gray-800 bg-opacity-50" />
+                </TransitionChild>
+
+                <div class="fixed inset-0 overflow-y-auto">
+                    <div
+                        class="flex items-center justify-center min-h-full p-4 text-center h-5/6"
+                    >
+                        <TransitionChild
+                            as="template"
+                            enter="duration-300 ease-out"
+                            enter-from="opacity-0 scale-95"
+                            enter-to="opacity-100 scale-100"
+                            leave="duration-200 ease-in"
+                            leave-from="opacity-100 scale-100"
+                            leave-to="opacity-0 scale-95"
+                        >
+                            <DialogPanel
+                                class="w-full max-w-6xl min-h-full p-6 overflow-hidden text-left align-middle transition-all transform bg-white shadow-xl rounded-2xl"
+                            >
+                                <form
+                                    @submit.prevent="onSubmitAddTarifForm()"
+                                    autocomplete="off"
+                                >
+                                    <DialogTitle
+                                        as="div"
+                                        class="flex items-center justify-between w-full"
+                                    >
+                                        <h3
+                                            class="text-lg font-medium leading-6 text-gray-800"
+                                        >
+                                            Ajouter un tarif à
+                                            <span class="text-blue-700">
+                                                {{ structure.name }}</span
+                                            >
+                                        </h3>
+                                        <button type="button">
+                                            <XCircleIcon
+                                                @click="emit('close')"
+                                                class="w-6 h-6 text-gray-600 hover:text-gray-800"
+                                            />
+                                        </button>
+                                    </DialogTitle>
+                                    <div class="w-full mt-2">
+                                        <div class="flex flex-col space-y-3">
+                                            <!-- titre -->
+                                            <div>
+                                                <label
+                                                    for="titre"
+                                                    class="block text-sm font-medium text-gray-700"
+                                                >
+                                                    Titre
+                                                </label>
+                                                <div
+                                                    class="flex mt-1 rounded-md"
+                                                >
+                                                    <input
+                                                        v-model="
+                                                            formAddTarif.titre
+                                                        "
+                                                        type="text"
+                                                        name="titre"
+                                                        id="titre"
+                                                        class="flex-1 block w-full placeholder-gray-400 placeholder-opacity-25 border-gray-300 rounded-md shadow-sm sm:text-sm"
+                                                        placeholder=""
+                                                        autocomplete="none"
+                                                    />
+                                                </div>
+                                                <div
+                                                    v-if="errors.titre"
+                                                    class="mt-2 text-xs text-red-500"
+                                                >
+                                                    {{ errors.titre }}
+                                                </div>
+                                            </div>
+                                            <!-- description -->
+                                            <div>
+                                                <label
+                                                    for="description"
+                                                    class="block text-sm font-medium text-gray-700"
+                                                >
+                                                    Description
+                                                </label>
+                                                <div class="mt-1">
+                                                    <textarea
+                                                        v-model="
+                                                            formAddTarif.description
+                                                        "
+                                                        id="description"
+                                                        name="description"
+                                                        rows="2"
+                                                        class="block w-full h-32 min-h-full mt-1 placeholder-gray-400 placeholder-opacity-50 border border-gray-300 rounded-md shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm"
+                                                        :class="{
+                                                            errors: 'border-red-500 focus:ring focus:ring-red-200',
+                                                        }"
+                                                        placeholder=""
+                                                        autocomplete="none"
+                                                    />
+                                                </div>
+                                                <div
+                                                    v-if="errors.description"
+                                                    class="mt-2 text-xs text-red-500"
+                                                >
+                                                    {{ errors.description }}
+                                                </div>
+                                            </div>
+                                            <div
+                                                class="flex items-center justify-start w-full space-x-2"
+                                            >
+                                                <Listbox
+                                                    v-model="
+                                                        selectedUniteeDuree
+                                                    "
+                                                >
+                                                    <div class="relative mt-1">
+                                                        <ListboxButton
+                                                            class="relative w-full py-2 pl-3 pr-10 text-left bg-white rounded-lg shadow-md cursor-default focus:outline-none focus-visible:border-indigo-500 focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-opacity-75 focus-visible:ring-offset-2 focus-visible:ring-offset-orange-300 sm:text-sm"
+                                                        >
+                                                            <span
+                                                                class="block truncate"
+                                                                >{{
+                                                                    selectedUniteeDuree.name
+                                                                }}</span
+                                                            >
+                                                            <span
+                                                                class="absolute inset-y-0 right-0 flex items-center pr-2 pointer-events-none"
+                                                            >
+                                                                <ChevronUpDownIcon
+                                                                    class="w-5 h-5 text-gray-400"
+                                                                    aria-hidden="true"
+                                                                />
+                                                            </span>
+                                                        </ListboxButton>
+
+                                                        <transition
+                                                            leave-active-class="transition duration-100 ease-in"
+                                                            leave-from-class="opacity-100"
+                                                            leave-to-class="opacity-0"
+                                                        >
+                                                            <ListboxOptions
+                                                                class="absolute w-full py-1 mt-1 overflow-auto text-base bg-white rounded-md shadow-lg max-h-60 ring-1 ring-black ring-opacity-5 focus:outline-none sm:text-sm"
+                                                            >
+                                                                <ListboxOption
+                                                                    v-slot="{
+                                                                        active,
+                                                                        selected,
+                                                                    }"
+                                                                    v-for="unitee in uniteDurees"
+                                                                    :key="
+                                                                        unitee.name
+                                                                    "
+                                                                    :value="
+                                                                        unitee
+                                                                    "
+                                                                    as="template"
+                                                                >
+                                                                    <li
+                                                                        :class="[
+                                                                            active
+                                                                                ? 'bg-amber-100 text-amber-900'
+                                                                                : 'text-gray-900',
+                                                                            'relative cursor-default select-none py-2 pl-10 pr-4',
+                                                                        ]"
+                                                                    >
+                                                                        <span
+                                                                            :class="[
+                                                                                selected
+                                                                                    ? 'font-medium'
+                                                                                    : 'font-normal',
+                                                                                'block truncate',
+                                                                            ]"
+                                                                            >{{
+                                                                                unitee.name
+                                                                            }}</span
+                                                                        >
+                                                                        <span
+                                                                            v-if="
+                                                                                selected
+                                                                            "
+                                                                            class="absolute inset-y-0 left-0 flex items-center pl-3 text-amber-600"
+                                                                        >
+                                                                            <CheckIcon
+                                                                                class="w-5 h-5"
+                                                                                aria-hidden="true"
+                                                                            />
+                                                                        </span>
+                                                                    </li>
+                                                                </ListboxOption>
+                                                            </ListboxOptions>
+                                                        </transition>
+                                                    </div>
+                                                </Listbox>
+                                            </div>
+                                        </div>
+                                    </div>
+
+                                    <div
+                                        class="flex items-center justify-between w-full mt-4"
+                                    >
+                                        <button
+                                            type="button"
+                                            class="inline-flex justify-center px-4 py-2 text-sm font-medium text-white bg-red-600 border border-transparent rounded-md hover:bg-red-700 focus:outline-none focus-visible:ring-2 focus-visible:ring-red-600 focus-visible:ring-offset-2"
+                                            @click="emit('close')"
+                                        >
+                                            Annuler
+                                        </button>
+                                        <button
+                                            :disabled="formAddTarif.processing"
+                                            type="submit"
+                                            class="inline-flex justify-center px-4 py-2 text-sm font-medium text-white bg-green-600 border border-transparent rounded-md hover:bg-green-700 focus:outline-none focus-visible:ring-2 focus-visible:ring-green-600 focus-visible:ring-offset-2"
+                                        >
+                                            Enregistrer
+                                        </button>
+                                    </div>
+                                </form>
+                            </DialogPanel>
+                        </TransitionChild>
+                    </div>
+                </div>
+            </Dialog>
+        </TransitionRoot>
+    </div>
+</template>
