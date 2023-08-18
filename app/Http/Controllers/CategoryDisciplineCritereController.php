d5ae411ed816625a62c658babec33d5d52933316
Chaloulou75 <c.jeandey@gmail.com>
2023-08-19 00:44:38 +0200

    admin disciplines criteres start3

--- app/Http/Controllers/CategoryDisciplineCritereController.php
+++ app/Http/Controllers/CategoryDisciplineCritereController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\LienDisciplineCategorieCritere;
+use App\Models\LienDisciplineCategorieCritereValeur;
 use Illuminate\Http\Request;
 
 class CategoryDisciplineCritereController extends Controller
@@ -12,15 +13,19 @@ class CategoryDisciplineCritereController extends Controller
      */
     public function destroy(LienDisciplineCategorieCritere $lienDisciplineCategorieCritere)
     {
-
         $user = auth()->user();
         $this->authorize('viewAdmin', $user);
 
-        $discCatCritere = LienDisciplineCategorieCritere::where('id', $lienDisciplineCategorieCritere->id)->firstOrFail();
-        dd($discCatCritere);
+        $discCatCritere = LienDisciplineCategorieCritere::with('valeurs')->where('id', $lienDisciplineCategorieCritere->id)->firstOrFail();
+
+        if($discCatCritere->valeurs->isNotEmpty()) {
+            foreach ($discCatCritere->valeurs as $valeur) {
+                $valeur->delete();
+            }
+        }
 
         $discCatCritere->delete();
-        return redirect()->back()->with('success', 'Critère supprimé');
+        return redirect()->back()->with('success', 'Critère et valeurs associés supprimés');
 
     }
 }
