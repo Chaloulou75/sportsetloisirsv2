2f1494a73a42c0ead6110017f23cf31f3ff7df78
Chaloulou75 <c.jeandey@gmail.com>
2023-12-11 15:33:28 +0100

    remove pwa

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -10,11 +10,6 @@
         <link rel="preconnect" href="https://fonts.bunny.net">
         <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
 
-        <!-- PWA  -->
-        <meta name="theme-color" content="#6777ef"/>
-        <link rel="apple-touch-icon" href="{{ asset('images/logosmall.png') }}">
-        <link rel="manifest" href="{{ asset('/manifest.json') }}">
-
         <!-- Scripts -->
         @routes
         @vite(['resources/js/app.js', "resources/js/Pages/{$page['component']}.vue"])
@@ -22,22 +17,5 @@
     </head>
     <body class="font-sans antialiased">
         @inertia
-        <script src="{{ asset('/sw.js') }}"></script>
-        <script>
-            if ("serviceWorker" in navigator) {
-                // Register a service worker hosted at the root of the
-                // site using the default scope.
-                navigator.serviceWorker.register("/sw.js").then(
-                (registration) => {
-                    console.log("Service worker registration succeeded:", registration);
-                },
-                (error) => {
-                    console.error(`Service worker registration failed: ${error}`);
-                },
-                );
-            } else {
-                console.error("Service workers are not supported.");
-            }
-        </script>
     </body>
 </html>
