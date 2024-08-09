3e0cf41fbd1eb65bd64904c06b68a212466347d7
Chaloulou75 <c.jeandey@gmail.com>
2024-08-09 16:45:25 +0200

    typeChamp seeder

--- database/seeders/TypeChampsTableSeeder.php
+++ database/seeders/TypeChampsTableSeeder.php
@@ -18,20 +18,20 @@ class TypeChampsTableSeeder extends Seeder
         $now = Carbon::now();
 
         $typeChamps = [
-            ['type' => 'select', 'criterable' => 'true', 'sous_criterable' => 'true', 'created_at' => $now],
-            ['type' => 'radio', 'criterable' => 'true', 'sous_criterable' => 'true', 'created_at' => $now],
-            ['type' => 'checkbox', 'criterable' => 'true', 'sous_criterable' => 'true', 'created_at' => $now],
-            ['type' => 'range', 'criterable' => 'true', 'sous_criterable' => 'false', 'created_at' => $now],
-            ['type' => 'range multiple', 'criterable' => 'true', 'sous_criterable' => 'false', 'created_at' => $now],
-            ['type' => 'text', 'criterable' => 'true', 'sous_criterable' => 'false', 'created_at' => $now],
-            ['type' => 'number', 'criterable' => 'true', 'sous_criterable' => 'false', 'created_at' => $now],
-            ['type' => 'date', 'criterable' => 'true', 'sous_criterable' => 'false', 'created_at' => $now],
-            ['type' => 'dates', 'criterable' => 'true', 'sous_criterable' => 'false', 'created_at' => $now],
-            ['type' => 'time', 'criterable' => 'true', 'sous_criterable' => 'false', 'created_at' => $now],
-            ['type' => 'times', 'criterable' => 'true', 'sous_criterable' => 'false', 'created_at' => $now],
-            ['type' => 'mois', 'criterable' => 'true', 'sous_criterable' => 'false', 'created_at' => $now],
-            ['type' => 'adresse', 'criterable' => 'false', 'sous_criterable' => 'false', 'created_at' => $now],
-            ['type' => 'instructeur', 'criterable' => 'false','sous_criterable' => 'false', 'created_at' => $now],
+            ['type' => 'select', 'criterable' => 1, 'sous_criterable' => 1, 'created_at' => $now],
+            ['type' => 'radio', 'criterable' => 1, 'sous_criterable' => 1, 'created_at' => $now],
+            ['type' => 'checkbox', 'criterable' => 1, 'sous_criterable' => 1, 'created_at' => $now],
+            ['type' => 'range', 'criterable' => 1, 'sous_criterable' => 0, 'created_at' => $now],
+            ['type' => 'range multiple', 'criterable' => 1, 'sous_criterable' => 0, 'created_at' => $now],
+            ['type' => 'text', 'criterable' => 1, 'sous_criterable' => 0, 'created_at' => $now],
+            ['type' => 'number', 'criterable' => 1, 'sous_criterable' => 0, 'created_at' => $now],
+            ['type' => 'date', 'criterable' => 1, 'sous_criterable' => 0, 'created_at' => $now],
+            ['type' => 'dates', 'criterable' => 1, 'sous_criterable' => 0, 'created_at' => $now],
+            ['type' => 'time', 'criterable' => 1, 'sous_criterable' => 0, 'created_at' => $now],
+            ['type' => 'times', 'criterable' => 1, 'sous_criterable' => 0, 'created_at' => $now],
+            ['type' => 'mois', 'criterable' => 1, 'sous_criterable' => 0, 'created_at' => $now],
+            ['type' => 'adresse', 'criterable' => 0, 'sous_criterable' => 0, 'created_at' => $now],
+            ['type' => 'instructeur', 'criterable' => 0,'sous_criterable' => 0, 'created_at' => $now],
         ];
 
         DB::table('type_champs')->insert($typeChamps);
